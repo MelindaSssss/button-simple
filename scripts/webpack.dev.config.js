6922e5d7456ae8543f9e0ecdc748b3bacbe759dc
施艳 <shiyan@datatom.com>
2021-08-26 17:43:32 +0800

    fix: 配置webpack路径别名 react相关

--- scripts/webpack.dev.config.js
+++ scripts/webpack.dev.config.js
@@ -3,14 +3,21 @@ const webpack = require("webpack");
 const webpackConfigBase = require("./webpack.base.config");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { merge } = require("webpack-merge");
-
+// const NODE_DIR = path.resolve(__dirname, "node_modules");
 function resolve(relatedPath) {
   return path.join(__dirname, relatedPath);
 }
 
 const webpackConfigDev = {
   mode: "development",
-
+  resolve: {
+    alias: {
+      // react: path.join(NODE_DIR, "react/dist/react.min.js"),
+      // "react-dom": path.join(NODE_DIR, "react-dom/dist/react-dom.min.js"),
+      React: "react",
+      ReactDOM: "react-dom",
+    },
+  },
   entry: {
     app: [resolve("../src/index.js")],
   },
--- src/components/ButtonSimple.js
+++ src/components/ButtonSimple.js
@@ -1,43 +1,39 @@
-// import React, { useState, memo } from "react";
-// import "./ButtonSimple.css";
-// const ButtonSimple = () => {
-//   // const [butttonText, setButtonText] = useState("login");
-//   let buttonText = "login";
-//   function setButtonText() {
-//     buttonText = buttonText === "login" ? "loginOut" : "login";
-//   }
-//   return (
-//     <div className="button-container" onClick={() => setButtonText}>
-//       <span>{buttonText}</span>
-//     </div>
-//   );
-// };
-// export default memo(ButtonSimple);
-
-import React, { Component, memo } from "react";
+import React, { useState, memo } from "react";
 import "./ButtonSimple.css";
-
-class ButtonSimple extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      btnTxt: "Login",
-    };
-  }
-
-  render() {
-    const { btnTxt } = this.state;
-    return (
-      <div
-        className="button-container"
-        onClick={() => {
-          this.setState({ btnTxt: btnTxt === "Login" ? "Logout" : "Login" });
-        }}
-      >
-        <span>{btnTxt}</span>
-      </div>
-    );
-  }
-}
-
+const ButtonSimple = () => {
+  const [buttonText, setButtonText] = useState("login");
+  return (
+    <div className="button-container" onClick={() => setButtonText}>
+      <span>{buttonText}</span>
+    </div>
+  );
+};
 export default memo(ButtonSimple);
+
+// import React, { Component, memo } from "react";
+// import "./ButtonSimple.css";
+
+// class ButtonSimple extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       btnTxt: "Login",
+//     };
+//   }
+
+//   render() {
+//     const { btnTxt } = this.state;
+//     return (
+//       <div
+//         className="button-container"
+//         onClick={() => {
+//           this.setState({ btnTxt: btnTxt === "Login" ? "Logout" : "Login" });
+//         }}
+//       >
+//         <span>{btnTxt}</span>
+//       </div>
+//     );
+//   }
+// }
+
+// export default memo(ButtonSimple);
